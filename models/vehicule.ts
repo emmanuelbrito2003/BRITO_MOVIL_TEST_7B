1b71eaa8cfaad471c0ad42c471111606611017f9
Emmanuel Brito <emmanuelbrito2003@gmail.com>
2025-12-01 17:39:19 -0500

    se genero el nuevo modelo vehiculo con los atributos correspondientes

---
+++ models/vehicule.ts
@@ -0,0 +1,20 @@
+export type vehicule = {
+    marca: string,
+    modelo: string,
+    placa: string,
+    nombreDueño: string,
+    año: number,
+    tipoGasolina: string,
+
+
+}
+
+
+export const emptyVehicule: vehicule = {
+    marca: "",
+    modelo: "",
+    placa: "",
+    nombreDueño: "",
+    año: 0,
+    tipoGasolina: "",
+}
